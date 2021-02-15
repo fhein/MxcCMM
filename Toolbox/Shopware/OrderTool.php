f08107bbf6253201eed8e7c3012ca3569b4e3ea7
frank.hein <frank.hein@maxence.de>
2021-02-15 20:15:00 +0100

    Members setOrderStatusDoctrine, setPaymentStatusDoctrine added. Use Doctrine functions to change the set the order status (trigger Doctrine events for Klarna support).

--- Toolbox/Shopware/OrderTool.php
+++ Toolbox/Shopware/OrderTool.php
@@ -3,11 +3,15 @@
 namespace MxcCommons\Toolbox\Shopware;
 
 use MxcCommons\Plugin\Service\DatabaseAwareTrait;
+use MxcCommons\Plugin\Service\ModelManagerAwareTrait;
 use MxcCommons\ServiceManager\AugmentedObject;
+use Shopware\Models\Order\Order;
+use Shopware\Models\Order\Status;
 
 class OrderTool implements AugmentedObject
 {
     use DatabaseAwareTrait;
+    use ModelManagerAwareTrait;
 
     public function getOrdersByOrderStatus(int $statusId)
     {
@@ -120,12 +124,28 @@ class OrderTool implements AugmentedObject
         );
     }
 
+    public function setOrderStatusDoctrine(int $orderId, int $statusId)
+    {
+        $order = $this->modelManager->getRepository(Order::class)->find($orderId);
+        $status = $this->modelManager->getReference(Status::class, $statusId);
+        $order->setOrderStatus($status);
+        $this->modelManager->flush($order);
+    }
+
     public function setOrderStatus(int $orderId, int $statusId)
     {
         $this->db->executeUpdate(
             'UPDATE s_order o SET o.status = :status WHERE o.id = :id',
             ['status' => $statusId, 'id' => $orderId]
         );
+    }
+
+    public function setPaymentStatusDoctrine(int $orderId, int $statusId)
+    {
+        $order = $this->modelManager->getRepository(Order::class)->find($orderId);
+        $status = $this->modelManager->getReference(Status::class, $statusId);
+        $order->setPaymentStatus($status);
+        $this->modelManager->flush($order);
     }
 
     public function setPaymentStatus(int $orderId, int $statusId)
